c712fe64ecdbae787c2aa8a4f208c19e35388359
alioufisa <alioufisa@gmail.com>
2023-06-04 16:27:02 +0000

    Build a navbar

--- game-fisa/src/App.tsx
+++ game-fisa/src/App.tsx
@@ -2,13 +2,20 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 import NavBar from './components/NavBar'
-import { Grid, GridItem } from '@chakra-ui/react'
+import { Grid, GridItem, Show } from '@chakra-ui/react'
 
 function App() {
 
-  return <Grid templateAreas={`"nav nav" "aside main"`}>
-    <GridItem area='nav' bg='coral'>Nav</GridItem>
-    <GridItem area='aside' bg='gold'>Aside</GridItem>
+  return <Grid templateAreas={{
+    base: `"nav" "main"`, 
+    lg:`"nav nav" "aside main"`
+  }}>
+    <GridItem area='nav' bg='coral'>
+      <NavBar />
+    </GridItem>
+    <Show above='lg'>
+      <GridItem area='aside' bg='gold'>Aside</GridItem>
+    </Show>
     <GridItem area='main' bg='dodgerblue'>main</GridItem>
   </Grid>
 }
--- game-fisa/src/components/NavBar.tsx
+++ game-fisa/src/components/NavBar.tsx
@@ -5,7 +5,7 @@ const NavBar = () => {
   return (
     <HStack>
         <Image src={logo} boxSize='60px'/>
-        <Text></Text>
+        <Text>NavBar</Text>
     </HStack>
   )
 }
