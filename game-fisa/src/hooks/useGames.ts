b620768acf6d0e532d0d36f67e2caf8122544e44
alioufisa <alioufisa@gmail.com>
2023-06-04 21:41:24 +0000

    Creating of Hook

---
+++ game-fisa/src/hooks/useGames.ts
@@ -0,0 +1,119 @@
+// import React, { useEffect, useState } from 'react'
+// import apiClient from '../services/api-client';
+
+
+
+// interface Game {
+//     id: number,
+//     name: string
+// }
+
+// interface FetchGamesResponse {
+//     count: number;
+//     results: Game[]
+// }
+
+// const useGames = () => {
+//     const [games, setGames] = useState<Game[]>([])
+//     const [error, setError] = useState('')
+
+//     useEffect(() => {
+
+//         const controller = new AbortController();
+
+//         apiClient.get<FetchGamesResponse>('/games', {signal: controller.signal})
+//      .then(res => setGames(res.data.results))
+//      .catch(err =>{
+//         if (err instanceof CanceledError) return;
+//         setError(error.message)}
+//         )
+
+//      return () => {
+//         controller.abort()
+//       }
+//     }, [])
+//     return { games, error }
+// }
+
+
+// export default useGames
+
+// import React, { useEffect, useState } from 'react';
+// import apiClient from '../services/api-client';
+
+// interface Game {
+//   id: number;
+//   name: string;
+// }
+
+// interface FetchGamesResponse {
+//   count: number;
+//   results: Game[];
+// }
+
+// const useGames = () => {
+//   const [games, setGames] = useState<Game[]>([]);
+//   const [error, setError] = useState('');
+
+//   useEffect(() => {
+//     const controller = new AbortController();
+
+//     apiClient
+//       .get<FetchGamesResponse>('/games', { signal: controller.signal })
+//       .then((res) => setGames(res.data.results))
+//       .catch((err) => {
+//         if (err.name !== 'AbortError') return; // Request was aborted, no need to handle the error
+//         setError('Failed to fetch games. Please try again later.');
+//       });
+
+//     return () => {
+//       controller.abort(); // Abort the request if the component is unmounted
+//     };
+//   }, []);
+
+//   return { games, error };
+// };
+
+// export default useGames;
+
+
+import React, { useEffect, useState } from 'react';
+import apiClient from '../services/api-client';
+
+interface Game {
+  id: number;
+  name: string;
+}
+
+interface FetchGamesResponse {
+  count: number;
+  results: Game[];
+}
+
+const useGames = () => {
+  const [games, setGames] = useState<Game[]>([]);
+  const [error, setError] = useState('');
+
+  useEffect(() => {
+    const controller = new AbortController();
+
+    apiClient
+      .get<FetchGamesResponse>('/games', { signal: controller.signal })
+      .then((res) => {
+        setGames(res.data.results);
+      })
+      .catch((err) => {
+        if (err.name === 'AbortError') {
+            setError('Failed to fetch games. Please try again later.');
+          }
+      });
+
+    return () => {
+      controller.abort();
+    };
+  }, []);
+
+  return { games, error };
+};
+
+export default useGames;
