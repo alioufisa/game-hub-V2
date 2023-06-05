815998236990add4c18e68e961ba1472231b728d
alioufisa <alioufisa@gmail.com>
2023-06-05 01:48:36 +0000

    Show loading skeletons

--- game-fisa/src/hooks/useGames.ts
+++ game-fisa/src/hooks/useGames.ts
@@ -102,18 +102,22 @@ interface FetchGamesResponse {
 const useGames = () => {
   const [games, setGames] = useState<Game[]>([]);
   const [error, setError] = useState('');
+  const [isLoading, setLoading] = useState(false)
 
   useEffect(() => {
     const controller = new AbortController();
 
+    setLoading(true);
     apiClient
       .get<FetchGamesResponse>('/games', { signal: controller.signal })
       .then((res) => {
         setGames(res.data.results);
+        setLoading(false)
       })
       .catch((err) => {
         if (err.name === 'AbortError') {
             setError('Failed to fetch games. Please try again later.');
+            setLoading(false)
           }
       });
 
@@ -122,7 +126,7 @@ const useGames = () => {
     };
   }, []);
 
-  return { games, error };
+  return { games, error, isLoading };
 };
 
 export default useGames;
