78947f4815b50c615bc48770e89a36e6b36ea00b
alioufisa <alioufisa@gmail.com>
2023-06-05 15:02:36 +0000

    Show a spinner while fetching the genres

--- game-fisa/src/components/GenreList.tsx
+++ game-fisa/src/components/GenreList.tsx
@@ -1,10 +1,14 @@
-import { HStack, List, ListItem, Image, Text } from '@chakra-ui/react'
+import { HStack, List, ListItem, Image, Text, Spinner } from '@chakra-ui/react'
 import useGenres, {Genre}  from '../hooks/useGenres'
 import getCroppedImageUrl from '../services/image-url'
 
 const GenreList = () => {
     
-    const  { data } = useGenres()
+    const  { data, isLoading, error } = useGenres()
+    if(error) return null
+    if (isLoading){
+        return <Spinner size='xl' />
+    }
   return (
     <List>
         {data.map(genre => <ListItem key={genre.id} paddingY='5px'>
