68ee93787a2bd6a68446a98c52554bbad23801c9
alioufisa <alioufisa@gmail.com>
2023-06-10 02:42:06 +0000

    Customize the theme to get darker grays

--- game-fisa/src/theme.ts
+++ game-fisa/src/theme.ts
@@ -4,6 +4,21 @@ const config: ThemeConfig = {
     initialColorMode: 'dark',
 }
 
-const theme = extendTheme({config})
+const theme = extendTheme({
+    config, 
+    colors: {
+        gray: {
+            100: '#f9f9f9',
+            200: '#ededed',
+            300: '#d3d3d3',
+            400: '#b3b3b3',
+            500: '#a0a0a0',
+            600: '#6c6c6c',
+            700: '#202020',
+            800: '#121212',
+            900: '#111',
+        }
+    }
+})
 
 export default theme;
