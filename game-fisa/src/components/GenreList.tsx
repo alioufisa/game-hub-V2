e6a68437c1c63618f56946721d4853ba010553eb
alioufisa <alioufisa@gmail.com>
2023-06-10 01:44:11 +0000

    Clean up the genres

--- game-fisa/src/components/GenreList.tsx
+++ game-fisa/src/components/GenreList.tsx
@@ -1,4 +1,4 @@
-import { HStack, List, ListItem, Image, Text, Spinner, Button } from '@chakra-ui/react'
+import { HStack, List, ListItem, Image, Text, Spinner, Button, Heading } from '@chakra-ui/react'
 import { useState } from 'react';
 import useGenres, {Genre}  from '../hooks/useGenres'
 import getCroppedImageUrl from '../services/image-url'
@@ -13,30 +13,36 @@ const GenreList = ({onSelectedGenre}: Props) => {
     const  { data, isLoading, error } = useGenres()
     if(error) return null
     if (isLoading){
-        return <Spinner size='xl' />
+      return <Spinner size='xl' />
     }
-
+    
     const handleGenreSelection = (genre: Genre) => {
-        setSelectedGenre(genre);
-        onSelectedGenre(genre);
-      };
-
-  return (
-    <List>
+      setSelectedGenre(genre);
+      onSelectedGenre(genre);
+    };
+    
+    return (
+    <>
+      <Heading fontSize='2xl'  marginBottom={3} >Genre</Heading>
+      <List>
         {data.map(genre => <ListItem key={genre.id} paddingY='5px'
         backgroundColor={selectedGenre?.id === genre.id ? 'yellow.500' : undefined} // Highlighting the selected genre
         >
             <HStack>
                 <Image borderRadius='8px' boxSize='32px' src={getCroppedImageUrl(genre.image_background)}/>
-                <Button
-              onClick={() => handleGenreSelection(genre)}
-              fontSize='lg'
-              variant='link'
-              fontWeight={selectedGenre?.id === genre.id ? 'bold' : undefined} // Bold font for selected genre
-            >{genre.name}</Button>
+                <Button 
+                whiteSpace='normal'
+                textAlign='left'
+                objectFit='cover'
+                onClick={() => handleGenreSelection(genre)}
+                fontSize='lg'
+                variant='link'
+                fontWeight={selectedGenre?.id === genre.id ? 'bold' : undefined} // Bold font for selected genre
+                >{genre.name}</Button>
             </HStack>
         </ListItem>) }
     </List>
+  </>
   )
 }
 
