c55e4094e5268d128e6e526a7f95bc69b90c0739
alioufisa <alioufisa@gmail.com>
2023-06-11 03:37:04 +0000

    All code review

--- game-fisa/src/components/FooterT.tsx
+++ game-fisa/src/components/FooterT.tsx
@@ -3,8 +3,8 @@
 
   export default function FooterT() {
     return (
-      <Box as="footer" bg="white">
-        <Container maxW="7xl" overflow="hidden" px={4} py={[20, 24]} mx="auto">
+      <Box as="footer" bg="white" borderRadius={10}>
+        <Container maxW="7xl" overflow="hidden" px={4} py={[20, 20]} mx="auto">
           <Text mt={5} textAlign="center" fontSize='xs' color="gray.500">
             &copy; 2023 Aliou Fisa, Inc. All rights reserved rawg.io
           </Text>
